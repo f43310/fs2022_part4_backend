4446ecb10dabb387755df94f1639134c6bd0e804
linda8850 <lijia9988@qq.com>
2022-06-27 11:42:07 +0800

    part4->exercises4.2 Extraction module.

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_BLOG_URI
+const Blog = require('./models/blog')
+mongoose
+  .connect(config.MONGODB_BLOG_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+const mongoUrl = config.MONGODB_BLOG_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
