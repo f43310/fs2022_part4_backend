bfba575edf6d8ab1094c4512b5b527ce467867b5
linda8850 <lijia9988@qq.com>
2022-06-27 12:50:35 +0800

    part4->exercises4.2 have done!

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
+const app = require('./app') // the actual Express application
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const Blog = require('./models/blog')
-mongoose
-  .connect(config.MONGODB_BLOG_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-const mongoUrl = config.MONGODB_BLOG_URI
-mongoose.connect(mongoUrl)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
