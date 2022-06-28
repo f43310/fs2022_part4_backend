9fe24ad021061a1bab5fdfa524171fc0de7c6f96
linda8850 <lijia9988@qq.com>
2022-06-28 22:48:47 +0800

    part4->exercises4.7 have done!

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -45,9 +45,57 @@ const mostBlogs = (blogs) => {
   return ret
 }
 
+const mostLikes = (blogs) => {
+  //先获取一下这个数组中有多少个type
+  var authors = []
+  for (var i in blogs) {
+    if (authors.indexOf(blogs[i].author) === -1) {
+      authors.push(blogs[i].author)
+    }
+  }
+  //一个包含多个list的结果对象
+  var obj = {}
+
+  // // 根据author生成多个数组
+  for (var k in authors) {
+    for (var j in blogs) {
+      if (blogs[j].author === authors[k]) {
+        console.log(authors[k], '>>>>>>>>>', blogs[j])
+        obj[authors[k]] = obj[authors[k]] || []
+        obj[authors[k]].push(blogs[j])
+      }
+    }
+  }
+  // console.log(authors)
+  // console.log(obj)
+
+  let arr2 = []
+  for (let x in obj) {
+    let obj2 = {}
+    let total = obj[x].reduce((a, b) => {
+      // console.log(a, b, b.likes)
+      return a + b.likes
+    }, 0)
+    // console.log(x, total, '==================')
+    obj2['author'] = x
+    obj2['likes'] = total
+    // console.log(obj2, '77777777777777777777')
+    arr2.push(obj2)
+  }
+  // console.log(arr2)
+
+  const obj3 = arr2.reduce((pre, cur) => {
+    return cur.likes > pre.likes ? cur : pre
+  })
+
+  // console.log(obj3)
+  return obj3
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 }
